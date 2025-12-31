8513a298f8ce71dd241856945c07508aa2e59c9d
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-12-30 23:24:55 -0500

    Start /bets endpoint

--- indexer/src/api/index.ts
+++ indexer/src/api/index.ts
@@ -1,13 +1,22 @@
-import { db } from "ponder:api";
-import schema from "ponder:schema";
-import { Hono } from "hono";
-import { client, graphql } from "ponder";
+import { Hono } from 'hono'
+import { client, desc, graphql, replaceBigInts } from 'ponder'
+import { db } from 'ponder:api'
+import schema from 'ponder:schema'
 
-const app = new Hono();
+const app = new Hono()
 
-app.use("/sql/*", client({ db, schema }));
+app.use('/sql/*', client({ db, schema }))
 
-app.use("/", graphql({ db, schema }));
-app.use("/graphql", graphql({ db, schema }));
+app.use('/', graphql({ db, schema }))
+app.use('/graphql', graphql({ db, schema }))
 
-export default app;
+app.get('/bets', async (c) => {
+  const bets = await db
+    .select()
+    .from(schema.bet)
+    .orderBy(desc(schema.bet.createdAt))
+
+  return c.json(replaceBigInts(bets, (b) => b.toString()))
+})
+
+export default app
