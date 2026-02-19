c8f5fda909393f28b5345b7214a14a65f543de63
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2026-02-18 23:19:39 -0700

    Use env var RPC only

--- indexer/ponder.config.ts
+++ indexer/ponder.config.ts
@@ -1,15 +1,20 @@
 import { parseAbiItem } from 'abitype'
 import { createConfig, factory } from 'ponder'
 import { BET_FACTORY_V1, BET_FACTORY_V2, BET_V1_ABI, BET_V2_ABI } from 'shared'
-import { fallback, webSocket } from 'viem'
 import { base } from 'viem/chains'
 
-const rpcUrls = ['https://base-rpc.publicnode.com', 'https://base.llamarpc.com']
+const rpcUrls: string[] = []
 
 const BASE_RPC_URL = process.env.BASE_RPC_URL
 
 if (BASE_RPC_URL) {
   rpcUrls.push(BASE_RPC_URL)
+} else {
+  const fallbackUrls = [
+    'https://base-rpc.publicnode.com',
+    'https://base.llamarpc.com',
+  ]
+  rpcUrls.push(...fallbackUrls)
 }
 
 export default createConfig({
@@ -17,7 +22,7 @@ export default createConfig({
     base: {
       id: base.id,
       rpc: rpcUrls,
-      ws: 'wss://base-rpc.publicnode.com', // process.env.BASE_WS_URL
+      ws: process.env.BASE_WS_URL,
     },
   },
   contracts: {
